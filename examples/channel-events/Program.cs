704a25fb572cab5a8a49b4676d504a83f49fe36c
Goncalo Oliveira <goncalo.a.oliveira@gmail.com>
2023-02-05 20:43:01 +0000

    updated examples

--- examples/channel-events/Program.cs
+++ examples/channel-events/Program.cs
@@ -3,59 +3,42 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
-var builder = new HostBuilder()
-    .ConfigureServices( ( context, services ) =>
+var builder = Host.CreateApplicationBuilder( args );
+
+builder.Services.AddChannelsHostedService( channels =>
+{
+    // configure options
+    channels.Configure( options =>
     {
-        // configure logging
-        services.AddLogging( loggingBuilder =>
-        {
-            loggingBuilder.AddSimpleConsole( options =>
-            {
-                options.IncludeScopes = true;
-                options.TimestampFormat = "hh:mm:ss ";
-            } )
-            .SetMinimumLevel( LogLevel.Information );
-        } );
-
-        // add our hosted service
-        services.AddChannelsHostedService( builder =>
-        {
-            // configure options
-            builder.Configure( options =>
-            {
-                options.Port = 8080;
-                options.Backlog = 10;
-            } );
-
-            // set up long-running services
-            // since v0.5 idle monitoring is a channel service
-            builder.AddIdleChannelService();
-
-            // set up input pipeline
-            /*
-            We need one at least one middleware component to "consume" the data
-            or the pipeline buffer will just keep growing
-            */
-            builder.AddInputHandler<VoidHandler>();
-
-            // set up output pipeline
-            /*
-            Nothing required
-            */            
-        } );
-
-        // set up channel event listeners
-        /*
-        We can add as many listeners as we want.
-        We're adding this one as singleton but that's not a requirement;
-        transient and scoped are valid as well, depending on our needs.
-        */
-        services.AddSingleton<Faactory.Channels.IChannelEvents, EventListener>();
-
-    } )
-    .UseConsoleLifetime( options =>
-    {
-        options.SuppressStatusMessages = true;
+        options.Port = 8080;
+        options.Backlog = 10;
     } );
 
-await builder.Build().RunAsync();
+    // set up long-running services
+    // since v0.5 idle monitoring is a channel service
+    channels.AddIdleChannelService();
+
+    // set up input pipeline
+    /*
+    We need one at least one middleware component to "consume" the data
+    or the pipeline buffer will just keep growing
+    */
+    channels.AddInputHandler<VoidHandler>();
+
+    // set up output pipeline
+    /*
+    Nothing required
+    */
+
+    // set up channel event listeners
+    /*
+    We can add as many listeners as we want.
+    We're adding this one as singleton but that's not a requirement;
+    transient and scoped are valid as well, depending on our needs.
+    */
+    channels.Services.AddSingleton<Faactory.Channels.IChannelEvents, EventListener>();
+} );
+
+var app = builder.Build();
+
+await app.RunAsync();
--- examples/echo-server/Program.cs
+++ examples/echo-server/Program.cs
@@ -1,52 +1,33 @@
-﻿using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
-var builder = new HostBuilder()
-    .ConfigureServices( ( context, services ) =>
+var builder = Host.CreateApplicationBuilder( args );
+
+builder.Services.AddChannelsHostedService( channels =>
+{
+    // configure options
+    channels.Configure( options =>
     {
-        // configure logging
-        services.AddLogging( loggingBuilder =>
-        {
-            loggingBuilder.AddSimpleConsole( options =>
-            {
-                options.IncludeScopes = false;
-                options.TimestampFormat = "hh:mm:ss";
-            } )
-            .SetMinimumLevel( LogLevel.Information );
-        } );
-
-        // add our hosted service
-        services.AddChannelsHostedService( builder =>
-        {
-            // configure options
-            builder.Configure( options =>
-            {
-                options.Port = 8080;
-                options.Backlog = 10;
-            } );
-
-            // set up long-running services
-            // since v0.5 idle monitoring is a channel service
-            builder.AddIdleChannelService();
-
-            // set up input pipeline
-            /*
-            We are replying the received data as it is, therefore we don't need adapters
-            */
-            builder.AddInputHandler<EchoHandler>();
-
-            // set up output pipeline
-            /*
-            We are replying the received data as it is, therefore we don't need adapters
-            */
-        } );
-
-    } )
-    .UseConsoleLifetime( options =>
-    {
-        options.SuppressStatusMessages = true;
+        options.Port = 8080;
+        options.Backlog = 10;
     } );
 
-await builder.Build().RunAsync();
+    // set up long-running services
+    // since v0.5 idle monitoring is a channel service
+    channels.AddIdleChannelService();
+
+    // set up input pipeline
+    /*
+    We are replying the received data as it is, therefore we don't need adapters
+    */
+    channels.AddInputHandler<EchoHandler>();
+
+    // set up output pipeline
+    /*
+    We are replying the received data as it is, therefore we don't need adapters
+    */
+} );
+
+var app = builder.Build();
+
+await app.RunAsync();
