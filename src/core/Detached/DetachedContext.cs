ed9d8552dccf63c261a4cd17c210b817a4e7728f
Goncalo Oliveira <goncalo.a.oliveira@gmail.com>
2023-11-01 13:57:50 +0000

    detached context now keeps track of output buffer and custom events

--- src/core/Detached/DetachedContext.cs
+++ src/core/Detached/DetachedContext.cs
@@ -5,15 +5,21 @@ namespace Faactory.Channels;
 /// <summary>
 /// An adapter context to be used outside a Channels context
 /// </summary>
-public sealed class DetachedContext : IAdapterContext
+public sealed class DetachedContext : IAdapterContext, IWritableBuffer
 {
     private readonly List<object> forwarded = new();
+    private readonly List<object> written = new();
+    private readonly List<KeyValuePair<string, object?>> customEvents = new();
 
     public IChannel Channel { get; } = new DetachedChannel();
 
-    public IWritableBuffer Output { get; } = new WritableBuffer();
+    public IWritableBuffer Output => this;
 
     public IEnumerable<object> Forwarded => forwarded.ToArray();
+
+    public IEnumerable<object> Written => written.ToArray();
+
+    public IEnumerable<KeyValuePair<string, object?>> CustomEvents => customEvents.ToArray();
 
     public void Forward( object data )
         => forwarded.Add( data );
@@ -25,15 +31,8 @@ public sealed class DetachedContext : IAdapterContext
         => forwarded.AddRange( collection );
 
     public void NotifyCustomEvent( string name, object? data = null )
-    {
-        throw new NotImplementedException();
-    }
+        => customEvents.Add( new( name, data ) );
 
-    private class WritableBuffer : IWritableBuffer
-    {
-        public void Write( object data )
-        {
-            // do nothing...
-        }
-    }
+    public void Write( object data )
+        => written.Add( data );
 }
