d6cd739fc89d14cfc4dbf084dbce9f7cdd0cb4d6
Goncalo Oliveira <goncalo.a.oliveira@gmail.com>
2026-02-23 15:49:46 +0100

    modified metric names

--- src/core/Metrics.cs
+++ src/core/Metrics.cs
@@ -10,20 +10,20 @@ internal static class Metrics
     } );
 
     public static readonly Counter<long> BytesReceived =
-        Meter.CreateCounter<long>( "bytes.received", "bytes", "Total number of bytes received" );
+        Meter.CreateCounter<long>( "channels.bytes_received", "bytes", "Total number of bytes received" );
 
     public static readonly Counter<long> BytesSent =
-        Meter.CreateCounter<long>( "bytes.sent", "bytes", "Total number of bytes sent" );
+        Meter.CreateCounter<long>( "channels.bytes_sent", "bytes", "Total number of bytes sent" );
 
     public static readonly UpDownCounter<long> ActiveChannels =
-        Meter.CreateUpDownCounter<long>( "active", description: "Number of active channels" );
+        Meter.CreateUpDownCounter<long>( "channels.active", description: "Number of active channels" );
 
     public static readonly Histogram<double> ChannelDuration =
-        Meter.CreateHistogram<double>( "duration", "ms", "Channel lifetime duration in milliseconds" );
+        Meter.CreateHistogram<double>( "channels.lifetime_duration", "ms", "Channel lifetime duration in milliseconds" );
 
     public static readonly Counter<long> MiddlewareExceptions =
-        Meter.CreateCounter<long>( "middleware.exceptions", description: "Total number of middleware exceptions" );
+        Meter.CreateCounter<long>( "channels.middleware_exceptions", description: "Total number of middleware exceptions" );
 
     public static readonly Counter<long> IdleTimeouts =
-        Meter.CreateCounter<long>( "idle.timeouts", description: "Total number of idle timeouts" );
+        Meter.CreateCounter<long>( "channels.idle_timeouts", description: "Total number of idle timeouts" );
 }
